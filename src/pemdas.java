c9a9342330ec25a7e5488dab281924f53bb18f35
jainkavya <jkavya865@gmail.com>
2025-06-24 14:14:27 +0530

    Increment decrement

---
+++ src/pemdas.java
@@ -0,0 +1,8 @@
+public class pemdas {
+    public static void main (String[] args) {
+
+        int result = 10 + 2 * 5 / 2 - 3;
+
+        System.out.println("The final result of: " + result);
+    }
+}
